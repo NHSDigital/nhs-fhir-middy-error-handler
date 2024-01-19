b394ac0597092edf931060cc94cfb3148ad98ea9
Jack Spagnoli <114365987+JackSpagnoliNHS@users.noreply.github.com>
2024-01-19 10:18:55 +0000

    tests running

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,36 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {JestConfigWithTsJest} from "ts-jest"
+
+const esModules = ["@middy"].join("|")
+const jestConfig: JestConfigWithTsJest = {
+  preset: "ts-jest/presets/default-esm",
+  moduleFileExtensions: ["js", "json", "ts", "d.ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1"
+  },
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "./tsconfig.json"
+      }
+    ]
+  },
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testMatch: ["**/tests/*.test.ts"],
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  verbose: true,
+  transformIgnorePatterns: [`node_modules/(?!${esModules})`],
+  "rootDir": "./"
+}
+
+export default jestConfig
